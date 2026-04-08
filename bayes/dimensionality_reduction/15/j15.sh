7ae1124853f8669e85b4b4df2a22ed8c1bfe7285
Jamin Hanna <jaminhanna@aol.com>
2026-04-07 21:28:38 -0400

    added test 1 for isi and sim time 8 using convolution

---
+++ bayes/dimensionality_reduction/15/j15.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#SBATCH --account=ISAAC-UTK0319
+#SBATCH --partition=campus
+#SBATCH --qos=campus
+#SBATCH --nodes=1
+#SBATCH --ntasks=41
+#SBATCH --time=24:00:00
+#SBATCH --output=j15.out
+#SBATCH --error=j15.err
+
+repos=/lustre/isaac24/scratch/jhanna8/repos
+dir=$repos/2026_dr_tests/bayes/dimensionality_reduction/15
+fr=$repos/framework
+cd $fr
+source pyframework/bin/activate
+if test ! -f bin/bayes
+then
+  make bin/bayes || exit 1
+fi
+cd cpp-apps
+cp $dir/dimensionality_reduction.json \
+   $dir/eons.json \
+   $dir/risp.json .
+../bin/bayes \
+  --input_file $dir/b15.in \
+  --output_file $dir/b15.out \
+  --n_calls 5 \
+  -d $dir/networks
+deactivate
+rm dimensionality_reduction.json eons.json risp.json
+cd $dir
