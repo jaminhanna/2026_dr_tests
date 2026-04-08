f1ec3475bc9349f2b43d9068a888122ec960eb4f
Jamin Hanna <jaminhanna@aol.com>
2026-04-07 21:22:36 -0400

    added test 3 for SPIKE using convolution

---
+++ bayes/dimensionality_reduction/13/j13.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#SBATCH --account=ISAAC-UTK0319
+#SBATCH --partition=campus
+#SBATCH --qos=campus
+#SBATCH --nodes=1
+#SBATCH --ntasks=41
+#SBATCH --time=24:00:00
+#SBATCH --output=j13.out
+#SBATCH --error=j13.err
+
+repos=/lustre/isaac24/scratch/jhanna8/repos
+dir=$repos/2026_dr_tests/bayes/dimensionality_reduction/13
+fr=$repos/framework
+cd $fr
+source pyframework/bin/activate
+if test ! -f bin/bayes
+then
+  make bin/bayes || exit 1
+fi
+cd cpp-apps
+cp $dir/dimensionality_reduction.json \
+   $dir/eons.json \
+   $dir/risp.json .
+../bin/bayes \
+  --input_file $dir/b13.in \
+  --output_file $dir/b13.out \
+  --n_calls 25 \
+  -d $dir/networks
+deactivate
+rm dimensionality_reduction.json eons.json risp.json
+cd $dir
